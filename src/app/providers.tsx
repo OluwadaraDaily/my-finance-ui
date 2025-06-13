84d850dcc0bd08d0c21d341c56d70efe5f30df75
Oluwadara Oloye <daraoloye99@gmail.com>
2025-06-13 11:04:10 +0100

    feat: enhance query client configuration with custom retry logic and stale time settings

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,12 +4,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 import { Toaster } from "@/components/ui/sonner"
 import { AuthProvider } from '@/context/AuthContext'
+import { AxiosError } from 'axios'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(
     () =>
       new QueryClient({
         defaultOptions: {
+          queries: {
+            staleTime: 5 * 60 * 1000,
+            gcTime: 10 * 60 * 1000,
+            refetchOnWindowFocus: false,
+            retry: (failureCount, error) => {
+              if (error instanceof AxiosError && error.response?.status === 404) return false;
+              return failureCount < 3;
+            },
+          },
           mutations: {
             retry: 1,
           },
